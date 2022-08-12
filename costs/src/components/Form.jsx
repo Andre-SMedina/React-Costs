e7ed0ce422aa6c630c12d6b2f880ab912585dd87
André Luís Silva Medina <106404962+Andre-SMedina@users.noreply.github.com>
2022-08-12 13:18:11 -0300

    fim

--- costs/src/components/Form.jsx
+++ costs/src/components/Form.jsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
+
 function Form() {
+  // André, é o valor padrão atribuido para name. setName é usado para atribuir um valor para name
+  const [name, setName] = useState("André");
+  const [password, setPassword] = useState();
+
   function cadastrarUsuario(e) {
     // evita recarregar a página depois do submit
     e.preventDefault();
+    console.log(`O usuário: ${name} foi cadastrado com a senha: ${password}`);
     console.log("Cadastrou o usuário!");
   }
 
@@ -10,7 +17,25 @@ function Form() {
       <h1>Meu Cadastro</h1>
       <form onSubmit={cadastrarUsuario}>
         <div>
-          <input type="text" placeholder="Digite o seu nome" />
+          <label htmlFor="name">Nome:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Digite o seu nome"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Senha:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Digite a sua senha"
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
         <div>
           <input type="submit" value="Cadastrar" />
