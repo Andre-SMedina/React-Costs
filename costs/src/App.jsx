c6caf2fd476307ea83d610b317b8bd3393c6da09
André Luís Silva Medina <106404962+Andre-SMedina@users.noreply.github.com>
2022-08-12 10:24:06 -0300

    fim-props

--- costs/src/App.jsx
+++ costs/src/App.jsx
@@ -1,11 +1,16 @@
 import "./App.css";
-import HelloWorld from "./components/HelloWorld";
+import Pessoa from "./components/Pessoa";
 
 function App() {
   return (
     <div className="App">
       <h1>Olá React</h1>
-      <HelloWorld />
+      <Pessoa
+        nome="Pedro"
+        idade="30"
+        profissao="Programador"
+        foto="https://i0.wp.com/multarte.com.br/wp-content/uploads/2019/03/250px-094gengar_tr5k.png?w=250&ssl=1"
+      />
     </div>
   );
 }
