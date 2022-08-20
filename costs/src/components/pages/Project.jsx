d6248d90c4531bbd9ad06f2bded3120695a0af2d
André Luís Silva Medina <106404962+Andre-SMedina@users.noreply.github.com>
2022-08-19 23:11:03 -0300

    add services

--- costs/src/components/pages/Project.jsx
+++ costs/src/components/pages/Project.jsx
@@ -1,10 +1,13 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { parse, v4 as uuidv4 } from "uuid";
 
 import styles from "./Project.module.css";
+
 import Loading from "../layouts/Loading";
 import Container from "../layouts/Container";
 import ProjectForm from "../project/ProjectForm";
+import ServiceForm from "../service/ServiceForm";
 import Message from "../layouts/Message";
 
 function Project() {
@@ -12,6 +15,7 @@ function Project() {
 
   const [project, setProject] = useState([]);
   const [showProjectForm, setShowProjectForm] = useState(false);
+  const [showServiceForm, setShowServiceForm] = useState(false);
   const [message, setMessage] = useState("");
   const [type, setType] = useState("");
 
@@ -30,10 +34,16 @@ function Project() {
   }, [id]);
 
   function editPost(project) {
+    setMessage("");
+
     //budget validation
     if (project.budget < project.cost) {
       setMessage("O orçamento não pode ser menor do que o custo do projeto!");
       setType("error");
+
+      setTimeout(() => {
+        setMessage("");
+      }, 2000);
 
       return false;
     }
@@ -51,16 +61,53 @@ function Project() {
         setShowProjectForm(false);
         setMessage("Projeto atualizado!");
         setType("success");
-        setTimeout(() => {
-          setMessage("");
-          setType("");
-        }, 2000);
+      })
+      .catch((err) => console.log(err));
+  }
+
+  function createService(project) {
+    //last service
+    const lastService = project.services[project.services.length - 1];
+    const lastServiceCost = lastService.cost;
+    const newCost = parseFloat(project.cost) + parseFloat(lastServiceCost);
+
+    lastService.id = uuidv4();
+
+    //maximun value validation
+    if (newCost > parseFloat(project.budget)) {
+      setMessage("Orçamento ultrapassado, verifique o valor do serviço");
+      setType("error");
+      project.services.pop();
+
+      setTimeout(() => {
+        setMessage("");
+      }, 2000);
+      return false;
+    }
+
+    //add service cost to project total cost
+    project.cost = newCost;
+
+    //update project
+    fetch(`http://localhost:5000/projects/${project.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(project),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        //view the services
       })
       .catch((err) => console.log(err));
   }
 
   function toggleProjectForm() {
     setShowProjectForm(!showProjectForm);
+  }
+  function toggleServiceForm() {
+    setShowServiceForm(!showServiceForm);
   }
 
   return (
@@ -98,6 +145,27 @@ function Project() {
                 </div>
               )}
             </div>
+            <div className={styles.service_form_container}>
+              <h2>Adicione um serviço:</h2>
+              <button className={styles.btn} onClick={toggleServiceForm}>
+                {!showServiceForm ? "Adicionar serviço" : "Fechar"}
+              </button>
+              <div className={styles.project_info}>
+                {showServiceForm && (
+                  <div className={styles.project_info_input}>
+                    <ServiceForm
+                      handleSubmit={createService}
+                      btnText="Adicionar Serviços"
+                      projectData={project}
+                    />
+                  </div>
+                )}
+              </div>
+            </div>
+            <h2>Serviços</h2>
+            <Container customClass="start">
+              <p>Itens de Serviços</p>
+            </Container>
           </Container>
         </div>
       ) : (
